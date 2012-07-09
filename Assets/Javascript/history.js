bc381cc2be6d696f13669e376946ffb4b42c9c85
Garrett Samm <garrett.samm@gmail.com>
2012-07-09 13:11:52 +0100

    Completed layout and history view

--- Assets/Javascript/history.js
+++ Assets/Javascript/history.js
@@ -1,1 +1,36 @@
-﻿
+﻿/* Draws a line-plot using the data in "weights" using the specified activityName as the key.
+* The second argument is the id of the canvas element to draw the graph in.
+*/
+function drawGraph(activityName, canvasId) {
+    var canvas = document.getElementById(canvasId);
+
+    if (canvas.getContext) {
+        var ctx1 = canvas.getContext("2d");
+        var ctx2 = canvas.getContext("2d");
+        var pixelsPerDate = canvas.width / (weights[activityName].length - 1);
+        var pixelsPerWeight = canvas.height / Math.max.apply(Math, weights[activityName]);
+
+        ctx1.beginPath();
+
+        for (i = 0; i < weights[activityName].length; i++) {
+            var x = i * pixelsPerDate;
+            var y = canvas.height - (weights[activityName][i] * pixelsPerWeight) / 2;
+
+            if (i == 0) {
+                ctx1.moveTo(x, y);
+            } else {
+                ctx1.lineTo(x, y);
+            }
+
+            if (i > 0 && i < weights[activityName].length - 1) {
+                ctx2.moveTo(x, y);
+                ctx2.arc(x, y, 2, 0, Math.PI * 2, true);
+            }
+        }
+
+        ctx1.lineJoin = "round";
+        ctx1.lineWidth = 2;
+        ctx1.strokeStyle = "#FF0000";
+        ctx1.stroke();
+    }
+};
--- Assets/Javascript/workout.js
+++ Assets/Javascript/workout.js
@@ -1,1 +1,48 @@
-﻿
+﻿$(document).ready(function () {
+    function collapse(activity, data) {
+        var heading = activity.find("h3");
+        var details = activity.children(".details");
+
+        activity.find("h3").text(activity.find("h3").text() + " (Complete)");
+
+        activity.animate({
+            color: "#555",
+            backgroundColor: "#212121"
+        }, 1000);
+
+        details.animate({
+            opacity: 0,
+            height: 0
+        }, 1000, function () {
+            $(this).css("display", "none");
+
+            if (data == "workout complete") {
+                location.reload(true);
+            }
+        });
+    }
+
+    $(".success").click(function () {
+        var activity = $(this).parents(".activity");
+        var form = $(this).siblings("form");
+        form.children("input[name='isFailure']").val("False");
+
+        $.post(completeActivityUrl, form.serialize(), function (data) {
+            collapse(activity, data);
+        });
+
+        return false;
+    });
+
+    $(".failure").click(function () {
+        var activity = $(this).parents(".activity");
+        var form = $(this).siblings("form");
+        form.children("input[name='isFailure']").val("True");
+
+        $.post(completeActivityUrl, form.serialize(), function (data) {
+            collapse(activity, data);
+        });
+
+        return false;
+    });
+});
